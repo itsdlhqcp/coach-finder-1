b90e7801a3fdc13493f05b3c39b204c41ea8c3e3
Natalie-Cardot <nataliecardot@gmail.com>
2021-03-15 17:29:50 -0700

    Register components in routes

--- src/router.js
+++ src/router.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 import CoachDetail from './pages/coaches/CoachDetail.vue';
 import CoachesList from './pages/coaches/CoachesList.vue';
-import CoachesRegistration from './pages/coaches/CoachesRegistration.vue';
+import CoachRegistration from './pages/coaches/CoachRegistration.vue';
 import ContactCoach from './pages/requests/ContactCoach.vue';
 import RequestsReceived from './pages/requests/RequestsReceived.vue';
 import NotFound from './pages/NotFound.vue';
@@ -11,19 +11,19 @@ const router = createRouter({
   history: createWebHistory(),
   routes: [
     // Just domain
-    { path: '/', redirect: '/coaches' }
-    { path: '/coaches', component: null },
+    { path: '/', redirect: '/coaches' },
+    { path: '/coaches', component: CoachesList },
     {
       path: '/coaches/:id',
-      component: null,
+      component: CoachDetail,
       children: [
-        { path: 'contact', component: null } // /coaches/c1/contact
+        { path: 'contact', component: ContactCoach } // /coaches/c1/contact
       ]
     },
-    { path: '/register', component: null },
+    { path: '/register', component: CoachRegistration },
     // Messages received through coach contact form
-    { path: '/requests', component: null },
-    { path: '/:notFound(.*)', component: null }
+    { path: '/requests', component: RequestsReceived },
+    { path: '/:notFound(.*)', component: NotFound }
   ]
 });
 
