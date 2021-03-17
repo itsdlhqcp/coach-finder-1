c7fccee6a6c265590367881d00199e7dd3fe5350
Natalie-Cardot <nataliecardot@gmail.com>
2021-03-17 11:28:04 -0700

    Wire up pages

---
+++ src/pages/coaches/CoachDetails.vue
@@ -0,0 +1,6 @@
+<template>
+  DETAILS FOR COACH
+  <!-- Note: contact is a child route of this coach details route; won't be loaded to replace what was on screen previously, but it instead needs a new, separate router view within parent component, and child components (here, the contact route) will be loaded in this router view -->
+  <router-view></router-view>
+  <router-link to="/coaches/c1/contact">Contact</router-link>
+</template>
--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-import CoachDetail from './pages/coaches/CoachDetail.vue';
+import CoachDetails from './pages/coaches/CoachDetails.vue';
 import CoachesList from './pages/coaches/CoachesList.vue';
 import CoachRegistration from './pages/coaches/CoachRegistration.vue';
 import ContactCoach from './pages/requests/ContactCoach.vue';
@@ -15,7 +15,7 @@ const router = createRouter({
     { path: '/coaches', component: CoachesList },
     {
       path: '/coaches/:id',
-      component: CoachDetail,
+      component: CoachDetails,
       children: [
         { path: 'contact', component: ContactCoach } // /coaches/c1/contact
       ]
