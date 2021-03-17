8c987ad5f78c7e7c92e2e2b4a894de47bb41fb5f
Natalie-Cardot <nataliecardot@gmail.com>
2021-03-16 17:51:29 -0700

    Add header component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,18 @@
 <template>
+  <the-header></the-header>
   <router-view></router-view>
 </template>
+
+<script>
+import TheHeader from './components/layout/TheHeader.vue';
+
+export default {
+  // Registering it as a local component (because it's only used here) in this root component file https://vuejs.org/v2/guide/components-registration.html#Local-Registration
+  components: {
+    TheHeader
+  }
+}
+</script>
 
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
