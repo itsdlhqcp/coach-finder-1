59fc320ea8d4c1149cdd56556a5f58a3624a4440
Natalie-Cardot <nataliecardot@gmail.com>
2021-03-19 07:41:07 -0700

    Add and use base components

---
+++ src/components/ui/BaseCard.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="card">
+    <!-- Using slot so card can be used as a wrapper around any content passed in by parent (a slot is like a placeholder for HTML content) -->
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 1rem;
+  margin: 2rem auto;
+  max-width: 40rem;
+}
+</style>
--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,22 +1,26 @@
 <template>
   <section>FILTER</section>
   <section>
-    <div class="controls">
-      <button>Refresh</button>
-      <router-link to="/register">Register as Coach</router-link>
-    </div>
-    <ul v-if="hasCoaches">
-      <coach-item
-      v-for="coach in filteredCoaches"
-      :key="coach.id" :id="coach.id"
-      :first-name="coach.firstName"
-      :last-name="coach.lastName"
-      :rate="coach.hourlyRate"
-      :areas="coach.areas"
-      >
-      </coach-item>
-    </ul>
-    <h3 v-else>No coaches found.</h3>
+    <base-card>
+      <div class="controls">
+        <base-button>Refresh</base-button>
+        <!-- Adding link prop automatically sets it to true - including the prop with no value will imply true https://v3.vuejs.org/guide/component-props.html#passing-a-boolean -->
+        <base-button link to="/register">Register as Coach</base-button>
+      </div>
+      <ul v-if="hasCoaches">
+        <coach-item
+        v-for="coach in filteredCoaches"
+        :key="coach.id"
+        :id="coach.id"
+        :first-name="coach.firstName"
+        :last-name="coach.lastName"
+        :rate="coach.hourlyRate"
+        :areas="coach.areas"
+        >
+        </coach-item>
+      </ul>
+      <h3 v-else>No coaches found.</h3>
+    </base-card>
   </section>
 </template>
 
