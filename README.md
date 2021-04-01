4ec7647b2796c26eb58cff9b8486f91649cc648a
Natalie-Cardot <nataliecardot@gmail.com>
2021-03-31 21:27:35 -0700

    Send PUT request to store coach data

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ A coach finder web app built with Vue 3.
 
 Vuex is used for managing data/state.
 
+Firebase is used for the backend.
+
 Course project for Maximilian Schwarzmüller's [Vue - The Complete Guide](https://www.udemy.com/course/vuejs-2-the-complete-guide/).
 
 To view this app, install dependencies with `npm i`, run `npm run serve`, and navigate to `http://localhost:8080`.
--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,8 +1,8 @@
 // Actions: Mutations are synchronous. If you want to update state asynchronously, use actions. Components should trigger actions which in turn trigger mutations. It's considered a good practice in general to put actions between components and mutations, even though components could could commit mutations themselves and it wouldn't be a problem if you only had synchronous code, to ensure that you never accidentally put asynchronous code into a mutation
 export default {
-  registerCoach(context, data) {
+  async registerCoach(context, data) {
+    const userId = context.rootGetters.userId;
     const coachData = {
-      id: context.rootGetters.userId,
       firstName: data.first,
       lastName: data.last,
       description: data.desc,
@@ -10,7 +10,25 @@ export default {
       areas: data.areas
     };
 
+    const response = await fetch(
+      `https://vue3-http-3d6fb-default-rtdb.firebaseio.com/coaches/${userId}.json`,
+      {
+        // PUT: data will be overwritten if it existed, or created if it didn't exist
+        method: 'PUT',
+        body: JSON.stringify(coachData)
+      }
+    );
+
+    // const responseData = await response.json();
+
+    if (!response.ok) {
+      // Error...
+    }
+
     // Commit mutation
-    context.commit('registerCoach', coachData);
+    context.commit('registerCoach', {
+      ...coachData,
+      id: userId
+    });
   }
 };
