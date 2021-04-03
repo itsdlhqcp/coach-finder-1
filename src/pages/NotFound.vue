a831c9b02e0b7df4d8f42e8a96415a976826f270
Natalie-Cardot <nataliecardot@gmail.com>
2021-04-03 11:58:37 -0700

    Add not found page

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,8 @@
+<template>
+  <section>
+    <base-card>
+      <h2>Page Not Found</h2>
+      <p>This page could not be found. View all our <router-link to="/coaches">coaches</router-link>.</p>
+    </base-card>
+  </section>
+</template>
