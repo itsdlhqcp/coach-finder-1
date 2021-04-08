1c2f8ecfa77f26a4ef60c990adebcdfd4fa38638
Natalie-Cardot <nataliecardot@gmail.com>
2021-04-08 13:42:05 -0700

    Use lazy loading

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { createApp } from 'vue';
+import { createApp, defineAsyncComponent } from 'vue';
 
 import router from './router.js';
 import store from './store/index.js';
@@ -7,7 +7,14 @@ import BaseCard from './components/ui/BaseCard.vue';
 import BaseButton from './components/ui/BaseButton.vue';
 import BaseBadge from './components/ui/BaseBadge.vue';
 import BaseSpinner from './components/ui/BaseSpinner.vue';
-import BaseDialog from './components/ui/BaseDialog.vue';
+// import BaseDialog from './components/ui/BaseDialog.vue';
+
+// Only download code when needed
+// Vue checks templates of components you're working with, and if none of the components uses this file, it's not downloaded
+// Can be used for local and global components and routes
+const BaseDialog = defineAsyncComponent(() =>
+  import('./components/ui/BaseDialog.vue')
+);
 
 const app = createApp(App);
 
--- src/router.js
+++ src/router.js
@@ -1,13 +1,21 @@
+import { defineAsyncComponent } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
 import CoachDetails from './pages/coaches/CoachDetails.vue';
 import CoachesList from './pages/coaches/CoachesList.vue';
-import CoachRegistration from './pages/coaches/CoachRegistration.vue';
-import ContactCoach from './pages/requests/ContactCoach.vue';
-import RequestsReceived from './pages/requests/RequestsReceived.vue';
+// import CoachRegistration from './pages/coaches/CoachRegistration.vue';
+// import ContactCoach from './pages/requests/ContactCoach.vue';
+// import RequestsReceived from './pages/requests/RequestsReceived.vue';
 import NotFound from './pages/NotFound.vue';
-import UserAuth from './pages/auth/UserAuth.vue';
+// import UserAuth from './pages/auth/UserAuth.vue';
 import store from './store/index.js';
+
+// Lazy loading (note: async components shouldn't be used for routes. This is format suggested by Vue Router docs https://next.router.vuejs.org/guide/advanced/lazy-loading.html)
+const CoachDetails = () => import('./pages/coaches/CoachDetails.vue');
+const CoachRegistration = () => import('./pages/coaches/CoachRegistration.vue');
+const ContactCoach = () => import('./pages/requests/ContactCoach.vue');
+const RequestsReceived = () => import('./pages/requests/RequestsReceived.vue');
+const UserAuth = () => import('./pages/auth/UserAuth.vue');
 
 const router = createRouter({
   history: createWebHistory(),
