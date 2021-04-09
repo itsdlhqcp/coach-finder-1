af940ba6b12d34e63bde2fc0191ed33b1c4abda3
Natalie-Cardot <nataliecardot@gmail.com>
2021-04-08 20:09:24 -0700

    Add mobile styling

--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -3,7 +3,7 @@
     <nav>
       <h1><router-link to="/">Find a Coach</router-link></h1>
       <ul>
-        <li><router-link to="/coaches">All Coaches</router-link></li>
+        <li><router-link to="/coaches">Coaches</router-link></li>
         <li v-if="isLoggedIn">
           <router-link to="/requests">Requests</router-link>
         </li>
--- src/components/ui/BaseCard.vue
+++ src/components/ui/BaseCard.vue
@@ -13,4 +13,16 @@
   margin: 2rem auto;
   max-width: 40rem;
 }
+
+/* Mobile - same as Bootstrap 4 col-sm */
+@media screen and (max-width: 767px) {
+  .card {
+    width: 90%;
+    margin-top: 1.5rem;
+  }
+
+  .card:last-of-type {
+    margin-bottom: 1.5rem;
+  }
+}
 </style>
